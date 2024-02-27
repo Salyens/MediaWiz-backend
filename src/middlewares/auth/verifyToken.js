3469b085a3dd91a6d3b5de0da9b4fd8537c0368e
Salyens <salyens@yandex.ru>
2024-02-27 19:41:09 +0400

    update main page

--- src/middlewares/auth/verifyToken.js
+++ src/middlewares/auth/verifyToken.js
@@ -6,12 +6,11 @@ const verifyToken = (req, res, next) => {
     return res.status(401).send({ message: "Unauthorized" });
 
   const [type, token] = headers.authorization.split(" ");
-  console.log("type, token: ", type, token);
+
   if (type !== "Bearer" || !token)
     return res.status(401).send({ message: "Unauthorized" });
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    console.log("decoded: ", decoded);
     req["user"] = decoded;
   } catch (_) {
     return res.status(401).send({ message: "Unauthorized" });
--- src/routes/AdminRouter.js
+++ src/routes/AdminRouter.js
@@ -5,6 +5,7 @@ const verifyToken = require("../middlewares/auth/verifyToken");
 
 router.post("/registration", AdminController.createAdmin);
 router.post("/login", AdminController.login);
+router.get("/me", [verifyToken], (req, res) => res.send(req.user));
 
 router.route("/").get([verifyToken], FeedbackController.getAll);
 
